e22edf3dad8e00ccb2cad6c872d9fa4a95efcd66
Seungjin1102 <seungjin3047@gmail.com>
2023-03-13 15:17:47 +0900

    feat: BleViewModel 스캔 기능 추가
    startScan(), stopScan() 메소드 추가

--- app/src/main/java/com/rb/caapplication/viewmodel/BleViewModel.kt
+++ app/src/main/java/com/rb/caapplication/viewmodel/BleViewModel.kt
@@ -1,12 +1,20 @@
 package com.rb.caapplication.viewmodel
 
-import androidx.lifecycle.ViewModel
+import android.util.Log
+import androidx.databinding.ObservableArrayMap
+import com.polidea.rxandroidble2.exceptions.BleScanException
+import com.polidea.rxandroidble2.scan.ScanFilter
+import com.polidea.rxandroidble2.scan.ScanResult
+import com.polidea.rxandroidble2.scan.ScanSettings
 import com.rb.caapplication.base.BaseViewModel
 import com.rb.domain.ble.DeviceEvent
 import com.rb.domain.usecase.DeviceConnectionEventUseCase
 import com.rb.domain.usecase.ScanBleDevicesUseCase
 import com.rb.domain.usecase.TestScanBleDevicesUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.Disposable
+import io.reactivex.schedulers.Schedulers
 import kotlinx.coroutines.flow.SharedFlow
 import kotlinx.coroutines.flow.asSharedFlow
 import javax.inject.Inject
@@ -21,5 +29,43 @@ class BleViewModel @Inject constructor(
     val deviceConnectionEvent: SharedFlow<DeviceEvent<Boolean>> =
         deviceConnectionEventUseCase.execute().asSharedFlow()
 
+    private var scanSubscription: Disposable? = null
+
+    var scanResults = ObservableArrayMap<String, ScanResult>()
+
+
     fun testScan() = testScanBleDevicesUseCase.execute()
+
+    fun startScan() {
+        Log.d("sband", "BleViewModel startScan()")
+        val settings: ScanSettings = ScanSettings.Builder().build()
+        val scanFilter: ScanFilter = ScanFilter.Builder().build()
+
+        scanResults.clear()
+
+        scanSubscription =
+            scanBleDevicesUseCase.execute(settings, scanFilter)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe({ scanResult ->
+                    addScanResult(scanResult)
+                }, {throwable ->
+                    if (throwable is BleScanException) {
+                        Log.d("sband", "BleViewModel BleScanException throwable: ${throwable}")
+                    } else {
+                        Log.d("sband", "BleViewModel BleScanException Unknown error")
+                    }
+                })
+    }
+
+    fun stopScan() {
+        scanSubscription?.dispose()
+    }
+
+    private fun addScanResult(result: ScanResult) {
+        val device = result.bleDevice
+        val deviceAddress = device.macAddress
+        scanResults[deviceAddress] = result
+    }
+
 }
